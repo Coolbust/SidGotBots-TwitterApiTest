111004c190cacedc52a3dddc02094124a49b12c1
Sidney MS Robinson Jr <51104100+Coolbust@users.noreply.github.com>
2019-09-14 17:49:19 -0700

    Create ScoreBot.py

---
+++ ScoreBot.py
@@ -0,0 +1,26 @@
+import tweepy
+
+# Code to Authenticate to Twiiter
+auth = tweepy.OAuthHandler("Your_Handler")
+auth.set_access_token("Your_Token")
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+try:
+    api.verify_credentials()
+    print("Auth OK")
+except:
+    print("Error during auth")
+
+
+try:
+   # api.update_status("Auto Test Update 02: Lemme get a taste")
+    # print("Update succesful")
+    print("test")
+   #Print my followers
+    print(api.followers(api.me()))
+except:
+    print("Update fail")
+
